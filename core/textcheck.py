70c83a238d3f26d9dfc34585ea02d6b84985cb51
Muhammet Burak Golec <mozartburak42@gmail.com>
2023-02-09 23:11:33 +0300

    Fix : Textcheck.py (Regex)

--- core/textcheck.py
+++ core/textcheck.py
@@ -2,8 +2,11 @@ import re
 
 def find_template_text(text):
     pattern = r"\$\{\{ [a-zA-Z0-9_]+ \}\}"
+    pattern_2 = r"\$\{\{[a-zA-Z0-9_]+\}\}"
     match = re.search(pattern, text)
-    if match:
+    match_2 = re.search(pattern_2, text)
+
+    if match or match_2:
         return True
     else:
         return False
